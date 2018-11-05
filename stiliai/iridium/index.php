dda0c31b82bfcece29022a3cb0254a814c646025
alspok <alspok@gmail.com>
2018-11-05 19:14:45 +0200

    .

--- stiliai/iridium/index.php
+++ stiliai/iridium/index.php
@@ -6,7 +6,6 @@
 -->
 <html>
 	<head>
-		<title>Iridium by TEMPLATED</title>
 		<?php header_info(); ?>
 		<link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
 		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
@@ -14,11 +13,9 @@
 		<script src="js/skel.min.js"></script>
 		<script src="js/skel-panels.min.js"></script>
 		<script src="js/init.js"></script> -->
-		<noscript>
-			<link rel="stylesheet" href="css/skel-noscript.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-desktop.css" />
-		</noscript>
+			<link rel="stylesheet" href="stiliai/<?php echo input(strip_tags($conf['Stilius'])); ?>/css/skel-noscript.css" />
+			<link rel="stylesheet" href="stiliai/<?php echo input(strip_tags($conf['Stilius'])); ?>/css/style.css" />
+			<link rel="stylesheet" href="stiliai/<?php echo input(strip_tags($conf['Stilius'])); ?>/css/style-desktop.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
 		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
 	</head>
@@ -30,8 +27,8 @@
 				
 				<!-- Logo -->
 				<div id="logo">
-					<h1><a href="<?php echo adresas(); ?>">.Iridium</a></h1>
-					<span>Design by TEMPLATED</span>
+					<h1><a href="<?php echo adresas(); ?>"><?php echo $conf['Pavadinimas']; ?></a></h1>
+					<span><?php echo $conf['Apie']; ?></span>
 				</div>
 				
 				<!-- Nav -->
@@ -87,105 +84,11 @@
 					
 					<!-- Sidebar -->
 					<div id="sidebar" class="4u">
-						<section>
-							<header>
-								<h2>Pellentesque vulputate</h2>
-							</header>
-							<!-- <ul class="style">
-								<li>
-									<p class="posted">August 11, 2002  |  (10 )  Comments</p>
-									<img src="images/pic04.jpg" alt="" />
-									<p class="text">Nullam non wisi a sem eleifend. Donec mattis libero eget urna. Pellentesque viverra enim.</p>
-								</li>
-								<li>
-									<p class="posted">August 11, 2002  |  (10 )  Comments</p>
-									<img src="images/pic05.jpg" alt="" />
-									<p class="text">Nullam non wisi a sem eleifend. Donec mattis libero eget urna. Pellentesque viverra enim.</p>
-								</li>
-								<li>
-									<p class="posted">August 11, 2002  |  (10 )  Comments</p>
-									<img src="images/pic06.jpg" alt="" />
-									<p class="text">Nullam non wisi a sem eleifend. Donec mattis libero eget urna. Pellentesque viverra enim.</p>
-								</li>
-							</ul> -->
-							<?php include ( "priedai/desines_blokai.php" ); ?>
-						</section>
+						<?php include ( "priedai/desines_blokai.php" ); ?>
 					</div>
 				</div>
 			</div>
 		</div>
-
-		<!-- Footer -->
-		<div id="featured">
-			<div class="container">
-				<div class="row">
-					<div class="4u">
-						<h2>Aenean elementum facilisis</h2>
-						<a href="#" class="image full"><img src="images/pic01.jpg" alt="" /></a>
-						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
-						<p><a href="#" class="button">More Details</a></p>
-					</div>
-					<div class="4u">
-						<h2>Fusce ultrices fringilla</h2>
-						<a href="#" class="image full"><img src="images/pic02.jpg" alt="" /></a>
-						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
-						<p><a href="#" class="button">More Details</a></p>
-					</div>
-					<div class="4u">
-						<h2>Etiam rhoncus volutpat erat</h2>
-						<a href="#" class="image full"><img src="images/pic03.jpg" alt="" /></a>
-						<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum. Quisque dictum. Pellentesque viverra vulputate enim.</p>
-						<p><a href="#" class="button">More Details</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-
-		<!-- Footer -->
-		<div id="footer">
-			<div class="container">
-				<div class="row">
-					<div class="4u">
-						<section>
-							<h2>Latest Posts</h2>
-							<ul class="default">
-								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
-								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
-								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
-								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
-								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
-							</ul>
-						</section>
-					</div>
-					<div class="4u">
-						<section>
-							<h2>Ultrices fringilla</h2>
-							<ul class="default">
-								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
-								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
-								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
-								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
-								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
-							</ul>
-						</section>
-					</div>
-					<div class="4u">
-						<section>
-							<h2>Aenean elementum</h2>
-							<ul class="default">
-								<li><a href="#">Pellentesque lectus gravida blandit</a></li>
-								<li><a href="#">Lorem ipsum consectetuer adipiscing</a></li>
-								<li><a href="#">Phasellus nibh pellentesque congue</a></li>
-								<li><a href="#">Cras vitae metus aliquam pharetra</a></li>
-								<li><a href="#">Maecenas vitae orci feugiat eleifend</a></li>
-							</ul>
-						</section>
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<!-- Copyright -->
 		<div id="copyright">
 			<div class="container">
